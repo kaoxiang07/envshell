549edf50dc3e6429c0edbc818e7d72a917bf560c
kaoxiang07 <kaoxiang07@qq.com>
2020-12-19 15:57:13 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 echo "-----------------Starting mount disks-----------------"
 raidmd0=$(fdisk -l | grep /dev/md0)
 raidmd127=$(fdisk -l | grep /dev/md127)
-if [ -z $raidmd0 -a $raidmd127 ]
+if [ -z "$raidmd0" -a "$raidmd127" ]
 then
 	mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
 	mkfs.ext4 /dev/md0
@@ -13,10 +13,10 @@ then
 	blkid=$(blkid | grep /dev/md0)
 	uuid=${blkid:18:36}
 	echo "/dev/disk/by-uuid/$uuid /mnt ext4 defaults 0 0" >> /etc/fstab
+	echo "-----------------Disks mounted-----------------"
 else
 	echo "-----------------Already mounted-----------------"
 fi
-echo "-----------------Disks mounted-----------------"
 
 #2.Configure env
 envirConfig=/root/.bashrc
@@ -62,43 +62,43 @@ then
 	echo "export FIL_PROOFS_PARENT_CACHE=/mnt/filecoin-parents" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep BELLMAN_CUSTOM_GPU) ]
+if [ -z "$(cat $envirConfig | grep BELLMAN_CUSTOM_GPU)" ]
 then
 	echo "export BELLMAN_CUSTOM_GPU="GeForce RTX 3080:8704"" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep RUST_LOG) ]
+if [ -z "$(cat $envirConfig | grep RUST_LOG)" ]
 then
 	echo "export RUST_LOG=info" >> $envirConfig
 	echo "export RUST_BACKTRACE=full" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep GO111MODULE) ]
+if [ -z "$(cat $envirConfig | grep GO111MODULE)" ]
 then
 	echo "export GO111MODULE=on" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep GOPROXY) ]
+if [ -z "$(cat $envirConfig | grep GOPROXY)" ]
 then
 	echo "export GOPROXY=https://goproxy.cn" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep FIL_PROOFS_SDR_PARENTS_CACHE_SIZE) ]
+if [ -z "$(cat $envirConfig | grep FIL_PROOFS_SDR_PARENTS_CACHE_SIZE)" ]
 then
 	echo "export FIL_PROOFS_SDR_PARENTS_CACHE_SIZE=1073741824" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep RUSTUP_DIST_SERVER) ]
+if [ -z "$(cat $envirConfig | grep RUSTUP_DIST_SERVER)" ]
 then
 	echo "export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep RUSTUP_UPDATE_ROOT) ]
+if [ -z "$(cat $envirConfig | grep RUSTUP_UPDATE_ROOT)" ]
 then
 	echo "export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup" >> $envirConfig
 fi
 
-if [ -z $(cat $envirConfig | grep FIL_PROOFS_MAXIMIZE_CACHING) ]
+if [ -z "$(cat $envirConfig | grep FIL_PROOFS_MAXIMIZE_CACHING)" ]
 then
 	echo "export FIL_PROOFS_MAXIMIZE_CACHING=1" >> $envirConfig
 	echo "export FIL_PROOFS_USE_GPU_COLUMN_BUILDER=1" >> $envirConfig
@@ -114,7 +114,7 @@ apt update
 apt install -y gcc git bzr jq pkg-config mesa-opencl-icd ocl-icd-opencl-dev ubuntu-drivers-common lrzsz
 
 checkgo=$(go version)
-if [ -z $(cat $checkgo | grep go1.15) ]
+if [ -z "$(cat $checkgo | grep go1.15)" ]
 then
 	wget https://gomirrors.org/dl/go/go1.15.6.linux-amd64.tar.gz
 	tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
