89f54c5669844deb01529092cedaf3f6a373886c
kaoxiang07 <kaoxiang07@qq.com>
2020-12-19 08:08:43 +0000

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 echo "-----------------Starting mount disks-----------------"
 raidmd0=$(fdisk -l | grep /dev/md0)
 raidmd127=$(fdisk -l | grep /dev/md127)
-if [ -z "$raidmd0" -a "$raidmd127" ]
+if [ -z "$raidmd0" ] && [ -z "$raidmd127" ]
 then
 	mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
 	mkfs.ext4 /dev/md0
