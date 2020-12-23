5b937ec807c51d60aa3d37d284c5a85d3eaf61f9
kaoxiang07 <kaoxiang07@qq.com>
2020-12-23 06:41:51 +0000

    add original

--- install-original.sh
+++ install-original.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #1.mount disk
-echo "-----------------Starting mount disks-----------------"
-raidmd0=$(fdisk -l | grep /dev/md0)
-raidmd127=$(fdisk -l | grep /dev/md127)
-if [ -z "$raidmd0" ] && [ -z "$raidmd127" ]
-then
-	mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
-	mkfs.ext4 /dev/md0
-	mkdir /mnt
-	mount /dev/md0 /mnt
-	blkid=$(blkid | grep /dev/md0)
-	uuid=${blkid:18:36}
-	echo "/dev/disk/by-uuid/$uuid /mnt ext4 defaults 0 0" >> /etc/fstab
-	echo "-----------------Disks mounted-----------------"
-else
-	echo "-----------------Already mounted-----------------"
-fi
+#echo "-----------------Starting mount disks-----------------"
+#raidmd0=$(fdisk -l | grep /dev/md0)
+#raidmd127=$(fdisk -l | grep /dev/md127)
+#if [ -z "$raidmd0" ] && [ -z "$raidmd127" ]
+#then
+#	mdadm --create --verbose /dev/md0 --level=0 --raid-devices=4 /dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1
+#	mkfs.ext4 /dev/md0
+#	mkdir /mnt
+#	mount /dev/md0 /mnt
+#	blkid=$(blkid | grep /dev/md0)
+#	uuid=${blkid:18:36}
+#	echo "/dev/disk/by-uuid/$uuid /mnt ext4 defaults 0 0" >> /etc/fstab
+#	echo "-----------------Disks mounted-----------------"
+#else
+#	echo "-----------------Already mounted-----------------"
+#fi
 
 #2.Configure env
 envirConfig=/root/.bashrc
@@ -26,43 +26,43 @@ echo "-----------------Starting env configuration-----------------"
 echo "91.189.88.142 archive.ubuntu.com" >> $etchosts
 if [ ! -d $fil_proofs_parameter_cache ]
 then
-	mkdir -p /mnt/filecoin-proof-parameters
-	echo "export FIL_PROOFS_PARAMETER_CACHE=/mnt/filecoin-proof-parameters" >> $envirConfig
+	mkdir -p /home/storage/filecoin-proof-parameters
+	echo "export FIL_PROOFS_PARAMETER_CACHE=/home/storage/filecoin-proof-parameters" >> $envirConfig
 fi
 
 if [ ! -d $lotus_storage_path ]
 then
-	mkdir -p /mnt/local/.lotusstorage
-	echo "export LOTUS_STORAGE_PATH=/mnt/local/.lotusstorage" >> $envirConfig
+	mkdir -p /home/storage/lotuswork/lotusstorage
+	echo "export LOTUS_STORAGE_PATH=/home/storage/lotuswork/lotusstorage" >> $envirConfig
 fi
 
 if [ ! -d $lotus_path ]
 then
-	mkdir -p /mnt/local/lotus
-	echo "export LOTUS_PATH=/mnt/local/lotus" >> $envirConfig
+	mkdir -p /home/storage/lotuswork/lotus
+	echo "export LOTUS_PATH=/home/storage/lotuswork/lotus" >> $envirConfig
 fi
 
 if [ ! -d $worker_path ]
 then
-	mkdir -p /mnt/local/.worker
-	echo "export WORKER_PATH=/mnt/local/.worker" >> $envirConfig
+	mkdir -p /home/storage/lotuswork/lotusworker
+	echo "export WORKER_PATH=/home/storage/lotuswork/lotusworker" >> $envirConfig
 fi
 
 if [ ! -d $tmpdir ]
 then
-	mkdir -p /mnt/local/tmp
-	echo "export TMPDIR=/mnt/local/tmp" >> $envirConfig
+	mkdir -p /home/storage/lotuswork/tmpdir
+	echo "export TMPDIR=/home/storage/lotuswork/tmpdir" >> $envirConfig
 fi
 
 if [ ! -d $fil_proofs_parent_cache ]
 then
-	mkdir -p /mnt/filecoin-parents
-	echo "export FIL_PROOFS_PARENT_CACHE=/mnt/filecoin-parents" >> $envirConfig
+	mkdir -p /home/storage/cache
+	echo "export FIL_PROOFS_PARENT_CACHE=/home/storage/cache" >> $envirConfig
 fi
 
 if [ -z "$(cat $envirConfig | grep BELLMAN_CUSTOM_GPU)" ]
 then
-	echo "export BELLMAN_CUSTOM_GPU="GeForce RTX 3080:8704"" >> $envirConfig
+	echo "export BELLMAN_CUSTOM_GPU="GeForce GTX 2080Ti:4352"" >> $envirConfig
 fi
 
 if [ -z "$(cat $envirConfig | grep RUST_LOG)" ]
@@ -130,7 +130,7 @@ mv /etc/apt/sources.list /etc/apt/sources.list.aliyun
 mv /home/envshell/sources.list.original /etc/apt/
 mv /etc/apt/sources.list.original /etc/apt/sources.list
 
-apt install -y nvidia-driver-455 ntpdate
+apt install -y nvidia-driver-440-server ntpdate
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 ntpdate ntp.aliyun.com
 echo "-----------------All finished!!-----------------"
